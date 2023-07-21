9cd9f1ac0f418cb206f55b4373c46fd62337bc46
michaelhaaf <michael.haaf@gmail.com>
2023-07-21 02:01:08 -0400

    Update site title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: "Application Development II",
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: "https://github.com/michaelhaaf/5A6-F23",
       },
       sidebar: [
         {
-          label: 'Guides',
+          label: "About",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { label: "This Site", link: "/about/this-site/" },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Assignments",
+          autogenerate: { directory: "assignments" },
         },
       ],
     }),
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
+  site: "https://michaelhaaf.github.io",
 });
