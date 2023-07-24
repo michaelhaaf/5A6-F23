e279f0414e704c90ac5cf2f651f127d005b26ced
michaelhaaf <michael.haaf@gmail.com>
2023-07-23 20:09:54 -0400

    add course outline, using markdoc for table formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,62 +1,68 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Application Development II",
-      logo: { light: "./src/assets/logo.svg", dark: "./src/assets/logo.svg" },
-      customCss: [ 
-        "./src/styles/style.css", 
-        "./src/fonts/font-faces.css", 
-      ],
-      lastUpdated: true,
-      social: {
-        github: "https://github.com/michaelhaaf/5A6-F23",
+  integrations: [starlight({
+    title: "Application Development II",
+    logo: {
+      light: "./src/assets/logo.svg",
+      dark: "./src/assets/logo.svg"
+    },
+    customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
+    lastUpdated: true,
+    social: {
+      github: "https://github.com/michaelhaaf/5A6-F23"
+    },
+    sidebar: [{
+      label: "About",
+      autogenerate: {
+        directory: "about"
       },
-      sidebar: [
-        {
-          label: "About",
-          autogenerate: { directory: "about" },
-          collapsed: true,
-        },
-        {
-          label: "Lectures",
-          autogenerate: { directory: "lectures" },
-          collapsed: true,
-        },
-        {
-          label: "Tutorials",
-          autogenerate: { directory: "tutorials" },
-          collapsed: true,
-        },
-        {
-          label: "Assignments",
-          autogenerate: { directory: "assignments" },
-          collapsed: true,
-        },
-        {
-          label: "Exams",
-          autogenerate: { directory: "exams" },
-          collapsed: true,
-        },
-        {
-          label: "Project",
-          autogenerate: { directory: "project" },
-          collapsed: true,
-        },
-        {
-          label: "Resources",
-          autogenerate: { directory: "resources" },
-          collapsed: true,
-        },
-      ],
-    }),
-  ],
+      collapsed: true
+    }, {
+      label: "Lectures",
+      autogenerate: {
+        directory: "lectures"
+      },
+      collapsed: true
+    }, {
+      label: "Tutorials",
+      autogenerate: {
+        directory: "tutorials"
+      },
+      collapsed: true
+    }, {
+      label: "Assignments",
+      autogenerate: {
+        directory: "assignments"
+      },
+      collapsed: true
+    }, {
+      label: "Exams",
+      autogenerate: {
+        directory: "exams"
+      },
+      collapsed: true
+    }, {
+      label: "Project",
+      autogenerate: {
+        directory: "project"
+      },
+      collapsed: true
+    }, {
+      label: "Resources",
+      autogenerate: {
+        directory: "resources"
+      },
+      collapsed: true
+    }]
+  }), markdoc()],
   site: "https://michaelhaaf.github.io",
   base: "5A6-F23",
   experimental: {
     assets: true
-  },
+  }
 });
