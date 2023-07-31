a3e54d608dc2dab4df8c8d1e2b0a001419593aa0
michaelhaaf <michael.haaf@gmail.com>
2023-07-31 17:03:38 -0400

    implement custom component override integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,68 +1,81 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import overrideIntegration from "./src/overrideIntegration.mjs";
 
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "Application Development II",
-    logo: {
-      light: "./src/assets/logo.svg",
-      dark: "./src/assets/logo.svg"
-    },
-    customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
-    lastUpdated: true,
-    social: {
-      github: "https://github.com/michaelhaaf/5A6-F23"
-    },
-    sidebar: [{
-      label: "About",
-      autogenerate: {
-        directory: "about"
+  integrations: [
+    overrideIntegration(),
+    starlight({
+      title: "Application Development II",
+      logo: {
+        light: "./src/assets/logo.svg",
+        dark: "./src/assets/logo.svg",
       },
-      collapsed: true
-    }, {
-      label: "Lectures",
-      autogenerate: {
-        directory: "lectures"
+      customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
+      lastUpdated: true,
+      social: {
+        github: "https://github.com/michaelhaaf/5A6-F23",
       },
-      collapsed: true
-    }, {
-      label: "Tutorials",
-      autogenerate: {
-        directory: "tutorials"
-      },
-      collapsed: true
-    }, {
-      label: "Assignments",
-      autogenerate: {
-        directory: "assignments"
-      },
-      collapsed: true
-    }, {
-      label: "Exams",
-      autogenerate: {
-        directory: "exams"
-      },
-      collapsed: true
-    }, {
-      label: "Project",
-      autogenerate: {
-        directory: "project"
-      },
-      collapsed: true
-    }, {
-      label: "Resources",
-      autogenerate: {
-        directory: "resources"
-      },
-      collapsed: true
-    }]
-  }), markdoc()],
+      sidebar: [
+        {
+          label: "About",
+          autogenerate: {
+            directory: "about",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Lectures",
+          autogenerate: {
+            directory: "lectures",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Tutorials",
+          autogenerate: {
+            directory: "tutorials",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Assignments",
+          autogenerate: {
+            directory: "assignments",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Exams",
+          autogenerate: {
+            directory: "exams",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Project",
+          autogenerate: {
+            directory: "project",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Resources",
+          autogenerate: {
+            directory: "resources",
+          },
+          collapsed: true,
+        },
+      ],
+    }),
+    markdoc(),
+  ],
   site: "https://michaelhaaf.github.io",
   base: "5A6-F23",
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
