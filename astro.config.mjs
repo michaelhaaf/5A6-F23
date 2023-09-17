8f5c653359d4230eb42f6db4c485fa296fe665fc
michaelhaaf <michael.haaf@gmail.com>
2023-09-17 14:55:45 -0400

    add expressive code plugin and implement for existing lectures

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,49 @@ import overrideIntegration from "./src/overrideIntegration.mjs";
 
 import markdoc from "@astrojs/markdoc";
 
+import expressiveCode from "astro-expressive-code";
+const expressiveCodeOpts = {
+  theme: ["rose-pine-moon"],
+}
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    overrideIntegration(),
-    starlight({
-      title: "Application Development II",
-      logo: {
-        light: "./src/assets/logo.svg",
-        dark: "./src/assets/logo.svg",
+  integrations: [overrideIntegration(), expressiveCode(expressiveCodeOpts), starlight({
+    title: "Application Development II",
+    logo: {
+      light: "./src/assets/logo.svg",
+      dark: "./src/assets/logo.svg"
+    },
+    customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
+    lastUpdated: true,
+    pagination: true,
+    social: {
+      gitlab: "https://moodle.johnabbott.qc.ca/course/view.php?id=452",
+      github: "https://github.com/michaelhaaf/5A6-F23"
+    },
+    sidebar: [{
+      label: "About",
+      autogenerate: {
+        directory: "about"
+      },
+      collapsed: false
+    }, {
+      label: "Lectures",
+      autogenerate: {
+        directory: "lectures"
+      },
+      collapsed: false
+    }, {
+      label: "Assignments",
+      autogenerate: {
+        directory: "assignments"
+      },
+      collapsed: false
+    }, {
+      label: "Project",
+      autogenerate: {
+        directory: "project"
       },
       customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
       lastUpdated: true,
