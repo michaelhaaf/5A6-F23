9877de3d3003bf182d80e916378f88ddef07f802
michaelhaaf <michael.haaf@gmail.com>
2023-09-17 15:11:22 -0400

    hopefully fix astroconfig bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import overrideIntegration from "./src/overrideIntegration.mjs";
-
 import markdoc from "@astrojs/markdoc";
 
 import expressiveCode from "astro-expressive-code";
@@ -48,49 +47,12 @@ export default defineConfig({
       autogenerate: {
         directory: "project"
       },
-      customCss: ["./src/styles/style.css", "./src/fonts/font-faces.css"],
-      lastUpdated: true,
-      pagination: true,
-      social: {
-        gitlab: "https://moodle.johnabbott.qc.ca/course/view.php?id=452",
-        github: "https://github.com/michaelhaaf/5A6-F23",
-      },
-      sidebar: [
-        {
-          label: "About",
-          autogenerate: {
-            directory: "about",
-          },
-          collapsed: false,
-        },
-        {
-          label: "Lectures",
-          autogenerate: {
-            directory: "lectures",
-          },
-          collapsed: false,
-        },
-        {
-          label: "Assignments",
-          autogenerate: {
-            directory: "assignments",
-          },
-          collapsed: false,
-        },
-        {
-          label: "Project",
-          autogenerate: {
-            directory: "project",
-          },
-          collapsed: false,
-        },
-      ],
-    }),
-    markdoc(),
-  ],
+      collapsed: false
+    },]
+  }), markdoc()],
   site: "https://michaelhaaf.github.io",
   base: "5A6-F23",
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
